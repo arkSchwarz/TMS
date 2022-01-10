a7772e11d91628428ac47b4f60c14d06bf07d68b
arkSchwarz <batuhanordek@hotmail.com>
2022-01-10 21:56:35 +0300

    Create Functions

--- src/main/java/com/company/test.java
+++ src/main/java/com/company/test.java
@@ -6,6 +6,73 @@ import java.sql.*;
 public class test {
 
 
+    public void READ() throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost:49702;databaseName=Test;integratedSecurity=true;");
+
+        Statement m_Statement = conn.createStatement();
+        String query = "SELECT ALL * FROM [User] WHERE user_id='3'";
+
+        ResultSet m_ResultSet = m_Statement.executeQuery(query);
+
+        while (m_ResultSet.next()) {
+            System.out.println(m_ResultSet.getString(1) + ", " + m_ResultSet.getString(2) + ", " + m_ResultSet.getString(3) +
+                    ","+m_ResultSet.getString(4) + "," + m_ResultSet.getString(5));
+        }
+
+
+    }
+
+    public void WRITE() throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost:49702;databaseName=Test;integratedSecurity=true;");
+
+        String sql = "INSERT INTO [User] (type, mail, password, name_surname) VALUES (?, ?, ?, ?)";
+
+        PreparedStatement statement = conn.prepareStatement(sql);
+        statement.setString(1, "Employee");
+        statement.setString(2, "david28@hotmail.com");
+        statement.setString(3, "9696");
+        statement.setString(4, "David Beckhem");
+
+        int rowsInserted = statement.executeUpdate();
+        if (rowsInserted > 0) {
+            System.out.println("A new user was inserted successfully!");
+        }
+
+    }
+
+    public void UPTADE() throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost:49702;databaseName=Test;integratedSecurity=true;");
+
+        String sql = "UPDATE [User] SET type=?, mail=?, password=?, name_surname =? WHERE user_id='1'";
+
+        PreparedStatement statement = conn.prepareStatement(sql);
+
+        statement.setString(1, "Employee");
+        statement.setString(2, "david28@hotmail.com");
+        statement.setString(3, "6666");
+        statement.setString(4, "David M. Beckhem");
+
+        int rowsUpdated = statement.executeUpdate();
+        if (rowsUpdated > 0) {
+            System.out.println("An existing user was updated successfully!");
+        }
+
+    }
+
+    public void DELETE() throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost:49702;databaseName=Test;integratedSecurity=true;");
+
+        String sql = "DELETE FROM [User] WHERE user_id='1'";
+
+        PreparedStatement statement = conn.prepareStatement(sql);
+
+
+        int rowsDeleted = statement.executeUpdate();
+        if (rowsDeleted > 0) {
+            System.out.println("A user was deleted successfully!");
+        }
+
+    }
 
     public static void main(String[] args) {
 
@@ -13,71 +80,9 @@ public class test {
 
         try {
 
-            String url = "jdbc:sqlserver://localhost:49702;databaseName=Test;integratedSecurity=true;";
-
-            conn = DriverManager.getConnection(url);
+            conn = DriverManager.getConnection("jdbc:sqlserver://localhost:49702;databaseName=Test;integratedSecurity=true;");
             if (conn != null) {
 
-
-                Statement m_Statement = conn.createStatement();
-                String query = "SELECT ALL * FROM [User] WHERE user_id='3'";
-
-                ResultSet m_ResultSet = m_Statement.executeQuery(query);
-
-                while (m_ResultSet.next()) {
-                    System.out.println(m_ResultSet.getString(1) + ", " + m_ResultSet.getString(2) + ", " + m_ResultSet.getString(3) +
-                            ","+m_ResultSet.getString(4) + "," + m_ResultSet.getString(5));
-                }
-
-
-
-
-
-                /* WRITE
-                String sql = "INSERT INTO [User] (type, mail, password, name_surname) VALUES (?, ?, ?, ?)";
-
-                PreparedStatement statement = conn.prepareStatement(sql);
-                statement.setString(1, "Employee");
-                statement.setString(2, "david28@hotmail.com");
-                statement.setString(3, "9696");
-                statement.setString(4, "David Beckhem");
-
-                int rowsInserted = statement.executeUpdate();
-                if (rowsInserted > 0) {
-                    System.out.println("A new user was inserted successfully!");
-                }
-
-                 */
-
-            /* UPTADE
-                String sql = "UPDATE [User] SET type=?, mail=?, password=?, name_surname =? WHERE user_id='1'";
-
-                PreparedStatement statement = conn.prepareStatement(sql);
-
-                statement.setString(1, "Employee");
-                statement.setString(2, "david28@hotmail.com");
-                statement.setString(3, "6666");
-                statement.setString(4, "David M. Beckhem");
-
-                int rowsUpdated = statement.executeUpdate();
-                if (rowsUpdated > 0) {
-                    System.out.println("An existing user was updated successfully!");
-                }
-
-        */
-
-                /* DELETE
-                String sql = "DELETE FROM [User] WHERE user_id='1'";
-
-                PreparedStatement statement = conn.prepareStatement(sql);
-                //statement.setString(4, "David M. Beckhem");
-
-                int rowsDeleted = statement.executeUpdate();
-                if (rowsDeleted > 0) {
-                    System.out.println("A user was deleted successfully!");
-                }
-
-                 */
 
 
             }
